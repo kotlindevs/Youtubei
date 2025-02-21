c38fd0064eb3a83fa7433e87121d3c5ebb79dd31
Like Google ~ Criminal <kotlindevslife@gmail.com>
2025-02-21 07:22:52 +0530

    six commit

--- Youtubei.r
+++ Youtubei.r
@@ -1,63 +1,86 @@
-library(jsonlite)
 library(httr)
+library(jsonlite)
 
 youtube_search <- "https://www.youtube.com/youtubei/v1/search"
+response_okay <- 200
 
 header <- add_headers(
   "Content-Type" = "application/json",
-  "X-Goog-Api-Key" = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8",
-  "prettyPrint" = "false"
+  "prettyPrint"  = "false"
 )
 
-user_io <- readline(prompt = "enter your keyword : ")
-
-yt_search <- function(search_key) {
-  body <- list(
-    context = list(
-      client = list(
-        clientName = "WEB",
-        clientVersion = "2.20220918"
-      )
-    ), query = search_key
-  )
-
-  fmt_body <- toJSON(body, auto_unbox = TRUE)
-  youtube <- POST(
-    url = youtube_search,
-    config = header,
-    body = fmt_body
-  )
-
-  if (status_code(youtube) == 200) {
-    videos <- content(youtube, as = "text", encoding = "UTF-8")
-    result <- fromJSON(videos)
-    contents <- result$contents
-    search_results <- contents$twoColumnSearchResultsRenderer
-    primary_contents <- search_results$primaryContents
-    section_list_renderer <- primary_contents$sectionListRenderer
-    contents <- section_list_renderer$contents
-    item_sec_list <- contents$itemSectionRenderer
-    contents <- item_sec_list$contents
-    for (content in contents) {
-      video_renderer <- content[["videoRenderer"]]
-      video_id <- video_renderer[["videoId"]]
-      # thumbnail <- video_renderer[["thumbnail"]]
-      # thumbnail_parse <- thumbnail$thumbnails
-      # video_image <- unlist(lapply(thumbnail_parse, function(df) {
-      #   df$url[df$width == 720]
-      # }))
-      # title <- video_renderer$title$runs
-      # video_title <- unlist(lapply(title, function(df) {
-      #   df$text
-      # }))
-      view_count <- video_renderer$viewCountText$simpleText
-      video_view_count <- unlist(lapply(view_count, function(df) {
-        df
-      }))
-      mydf <- data.frame(video_id, video_view_count)
-      print(mydf)
-    }
-  }
+contents <- function(result) {
+  contents <- result[["contents"]]
+  two_col_search_result <- contents[["twoColumnSearchResultsRenderer"]]
+  primary_con <- two_col_search_result[["primaryContents"]]
+  section_lst_render <- primary_con[["sectionListRenderer"]]
+  section_lst_render
 }
 
-yt_search(user_io)
+section_list <- function(result) {
+  continuations <- result$continuations
+  next_continuation <- continuations[["nextContinuationData"]]
+  continuation <- next_continuation$continuation
+  continuation
+}
+
+body <- list(
+  context = list(
+    client = list(
+      clientName = "WEB",
+      clientVersion = "1.20241111.01.00"
+    )
+  ), query = "Bhagwan mahavir college surat",
+  continuation = ""
+)
+
+format_body <- toJSON(
+  body,
+  auto_unbox = TRUE,
+  pretty = TRUE
+)
+
+youtube <- POST(
+  url = youtube_search,
+  config = header,
+  body = format_body
+)
+
+temp_vec <- c()
+
+if (status_code(youtube) == response_okay) {
+  search_response <- content(youtube, as = "text", encoding = "UTF-8")
+  result <- fromJSON(search_response)
+  content <- contents(result = result)
+  section_lst <- section_list(result = content)
+  next_token <- section_lst
+
+  if (!is.null(section_lst) && next_token == section_lst) {
+    while (TRUE) {
+      body$continuation <- content
+
+      youtube <- POST(
+        url = youtube_search,
+        config = header,
+        body = format_body
+      )
+
+      if (status_code(youtube) == response_okay) {
+        search_response <- content(youtube, as = "text", encoding = "UTF-8")
+        result <- fromJSON(search_response)
+        content <- contents(result = result)
+        section_lst <- section_list(result = content)
+        next_token <- section_lst
+        temp_vec <- c(temp_vec, next_token)
+        if (is.null(section_lst)) {
+          break()
+        }
+      }
+
+      if (is.null(body$continuation)) {
+        break()
+      }
+    }
+    print(temp_vec)
+  }
+}
