57f5b03747040b0628457bb9c0ff2c11e1122343
Like Google ~ Criminal <kotlindevslife@gmail.com>
2025-02-22 19:03:50 +0530

    ninth commit

--- Youtubei.r
+++ Youtubei.r
@@ -1,15 +1,83 @@
 library(httr)
 library(jsonlite)
 
-youtube_search <- "https://www.youtube.com/youtubei/v1/search"
-response_okay <- 200
+youtube_token <- function(yt_url, yt_query, yt_params, yt_body) {
+  tokens <- c()
+  fetch_continuation <- function(response) {
+    if ("contents" %in% names(response)) {
+      return(response[["contents"]][["twoColumnSearchResultsRenderer"]]
+             [["primaryContents"]][["sectionListRenderer"]][["contents"]]
+             [["continuationItemRenderer"]][["continuationEndpoint"]]
+             [["continuationCommand"]][["token"]])
+    }
 
-header <- add_headers(
-  "Content-Type" = "application/json",
-  "prettyPrint"  = "false"
-)
+    if ("onResponseReceivedCommands" %in% names(response)) {
+      return(response[["onResponseReceivedCommands"]] #nolint
+             [["appendContinuationItemsAction"]]
+             [["continuationItems"]][[1]][["continuationItemRenderer"]]
+             [["continuationEndpoint"]][["continuationCommand"]][["token"]])
+    }
+  }
 
-next_token <- ""
+  youtube <- POST(
+    url = yt_url,
+    config = header,
+    body = yt_body
+  )
+
+  result <- fromJSON(content(youtube, as = "text", encoding = "UTF-8"))
+  token <- fetch_continuation(result)[2]
+
+  if (!is.na(token)) {
+    next_token <- token
+    duplicated_token <- 1
+    i <- 1
+    while (TRUE) {
+      body <- list(
+        context = list(
+          client = list(
+            clientName = "WEB",
+            clientVersion = "2.20250219.07.00"
+          )
+        ), query = yt_query,
+        continuation = token,
+        params = yt_params
+      )
+      format_body <- toJSON(
+        body,
+        auto_unbox = TRUE,
+        pretty = TRUE
+      )
+      yt <- POST(
+        url = yt_url, config = header, body = format_body
+      )
+
+      if (status_code(yt) == response_okay) {
+        res <- content(yt, as = "text", encoding = "UTF-8")
+        to_lst <- fromJSON(res)
+        token <- fetch_continuation(to_lst)[2]
+        if (!is.null(token)) {
+          if (next_token != token) {
+            tokens <- c(tokens, token)
+            i <- i + 1
+            next_token <- token
+          } else {
+            print(duplicated_token)
+            duplicated_token <- duplicated_token + 1
+          }
+        } else {
+          break()
+        }
+      } else {
+        print("Response not okay !")
+      }
+    }
+    return(tokens) #nolint
+  } else {
+    print("No token found !")
+  }
+}
+
 
 videos <- data.frame(
   video_id = character(), video_title = character(),
@@ -18,137 +86,147 @@ videos <- data.frame(
   short_views = character()
 )
 
+video_renderer <- function(video_renderer) {
+  video_id <- video_renderer$videoId
+  video_id <- unlist(lapply(video_id, function(video) {
+    video <- na.omit(video)
+    video
+  }))
+  channel_id <- video_renderer$longBylineText$runs
+  channel_id <- unlist(lapply(channel_id, function(video) {
+    video$navigationEndpoint$browseEndpoint$browseId
+  }))
+  channel_title <- video_renderer$longBylineText$runs
+  channel_title <- unlist(lapply(channel_title, function(channel) {
+    channel$text
+  }))
+  published_time <- video_renderer$publishedTimeText$simpleText
+  published_time <- unlist(lapply(published_time, function(pub_time) {
+    pub_time <- na.omit(pub_time)
+    pub_time
+  }))
+  video_length <- video_renderer$lengthText$simpleText
+  video_length <- unlist(lapply(video_length, function(video_len) {
+    video_len <- na.omit(video_len)
+    video_len
+  }))
+  short_views <- video_renderer$shortViewCountText$simpleText
+  short_views <- unlist(lapply(short_views, function(views) {
+    views <- na.omit(views)
+    views
+  }))
+  video_title <- video_renderer$title$runs
+  video_title <- unlist(lapply(video_title, function(vtitle) {
+    vtitle <- vtitle$text
+    vtitle
+  }))
+
+  max_df <- max(
+    length(video_id),
+    length(channel_title),
+    length(video_title),
+    length(channel_title),
+    length(short_views),
+    length(video_length),
+    length(published_time)
+  )
+
+  if (length(published_time) < max_df) {
+    length(published_time) <- max_df
+  } else if (length(short_views) < max_df) {
+    length(short_views) <- max_df
+  } else if (length(published_time) < max_df) {
+    length(published_time) <- max_df
+  } else if (length(channel_id) < max_df) {
+    length(channel_id) <- max_df
+  } else if (length(video_id) < max_df) {
+    length(video_id) <- max_df
+  } else if (length(channel_title) < max_df) {
+    length(channel_title) <- max_df
+  } else if (length(video_title) < max_df) {
+    length(video_title) <- max_df
+  }
+
+  df <- data.frame(
+    video_id, video_title, channel_id,
+    channel_title, published_time,
+    video_length, short_views
+  )
+  return(df) #nolint
+}
+
 contents <- function(result) {
   if ("contents" %in% names(result)) {
     contents <- result[["contents"]]
-    two_col_search_result <- contents[["twoColumnSearchResultsRenderer"]]
-    primary_con <- two_col_search_result[["primaryContents"]]
-    section_lst_render <- primary_con[["sectionListRenderer"]]
-    return(section_lst_render)
+    two_col_search_res <- contents[["twoColumnSearchResultsRenderer"]]
+    primary_con <- two_col_search_res[["primaryContents"]]
+    section_lst_ren <- primary_con[["sectionListRenderer"]]
+    contents <- section_lst_ren[["contents"]]
+    item_sec_ren <- contents[["itemSectionRenderer"]]
+    contents <- item_sec_ren[["contents"]]
+    item_section_renderer(results = contents)
   }
-  if ("continuationContents" %in% names(result)) {
-    continuation_contents <- result[["continuationContents"]]
-    sec_lst_continuation <- continuation_contents$sectionListContinuation
-    sec_lst_continuation
+
+  if ("onResponseReceivedCommands" %in% names(result)) {
+    on_response_rec_cmd <- result[["onResponseReceivedCommands"]] 
+    append_con_item <- on_response_rec_cmd[["appendContinuationItemsAction"]]
+    continuation_items <- append_con_item[["continuationItems"]][[1]]
+    item_section_renderer <- continuation_items[["itemSectionRenderer"]]
+    contents <- item_section_renderer[["contents"]]
+    item_section_renderer(results = contents)
   }
 }
 
-section_list <- function(result) {
-  continuations <- result$continuations
-  next_continuation <- continuations[["nextContinuationData"]]
-  continuation <- next_continuation$continuation
-  continuation
-}
-
-contents_result <- function(result) {
-  contents <- result[["contents"]]
-  item_sec_render <- contents[["itemSectionRenderer"]]
-  contents <- item_sec_render$contents
-  for (content in contents) {
-    if ("videoRenderer" %in% names(content)) {
-      video_renderer <- content$videoRenderer
-      video_id <- video_renderer$videoId
-      video_id <- unlist(lapply(video_id, function(video) {
-        video <- na.omit(video)
-        video
-      }))
-      channel_id <- video_renderer$longBylineText$runs
-      channel_id <- unlist(lapply(channel_id, function(video) {
-        video$navigationEndpoint$browseEndpoint$browseId
-      }))
-      channel_title <- video_renderer$longBylineText$runs
-      channel_title <- unlist(lapply(channel_title, function(channel) {
-        channel$text
-      }))
-      published_time <- video_renderer$publishedTimeText$simpleText
-      published_time <- unlist(lapply(published_time, function(pub_time) {
-        pub_time <- na.omit(pub_time)
-        pub_time
-      }))
-      video_length <- video_renderer$lengthText$simpleText
-      video_length <- unlist(lapply(video_length, function(video_len) {
-        video_len <- na.omit(video_len)
-        video_len
-      }))
-      short_views <- video_renderer$shortViewCountText$simpleText
-      short_views <- unlist(lapply(short_views, function(views) {
-        views <- na.omit(views)
-        views
-      }))
-      video_title <- video_renderer$title$runs
-      video_title <- unlist(lapply(video_title, function(vtitle) {
-        vtitle <- vtitle$text
-        vtitle
-      }))
-      df <- data.frame(
-        video_id, video_title, channel_id,
-        channel_title, published_time, video_length, short_views
-      )
-      if (nrow(df) > 0) {
-        videos <- rbind(videos, df)
+item_section_renderer <- function(results) {
+  for (result in results) {
+    if ("videoRenderer" %in% names(result)) {
+      video_ren <- result$videoRenderer
+      v <- video_renderer(video_renderer = video_ren)
+      if (nrow(v) > 0) {
+        videos <- rbind(videos, v)
       }
     }
-    if ("shelfRenderer" %in% names(content)) {
-      self_renderer <- content$shelfRenderer
-      content <- self_renderer$content
-      vert_lst_renderer <- content$verticalListRenderer
-      items <- vert_lst_renderer$items
+
+    if ("shelfRenderer" %in% names(result)) {
+      shelf_renderer <- result$shelfRenderer
+      content <- shelf_renderer$content
+      vertical_list_renderer <- content$verticalListRenderer
+      items <- vertical_list_renderer$items
       for (item in items) {
-        video_renderer <- item$videoRenderer
-        video_id <- video_renderer$videoId
-        video_id <- unlist(lapply(video_id, function(video) {
-          video <- na.omit(video)
-          video
-        }))
-        channel_id <- video_renderer$longBylineText$runs
-        channel_id <- unlist(lapply(channel_id, function(video) {
-          video$navigationEndpoint$browseEndpoint$browseId
-        }))
-        channel_title <- video_renderer$longBylineText$runs
-        channel_title <- unlist(lapply(channel_title, function(channel) {
-          channel$text
-        }))
-        published_time <- video_renderer$publishedTimeText$simpleText
-        published_time <- unlist(lapply(published_time, function(pub_time) {
-          pub_time <- na.omit(pub_time)
-          pub_time
-        }))
-        video_length <- video_renderer$lengthText$simpleText
-        video_length <- unlist(lapply(video_length, function(video_len) {
-          video_len <- na.omit(video_len)
-          video_len
-        }))
-        short_views <- video_renderer$shortViewCountText$simpleText
-        short_views <- unlist(lapply(short_views, function(views) {
-          views <- na.omit(views)
-          views
-        }))
-        video_title <- video_renderer$title$runs
-        video_title <- unlist(lapply(video_title, function(vtitle) {
-          vtitle <- vtitle$text
-          vtitle
-        }))
-        df <- data.frame(
-          video_id, video_title, channel_id,
-          channel_title, published_time,
-          video_length, short_views
-        )
-        if (nrow(df) > 0) {
-          videos <- rbind(videos, df)
+        if ("videoRenderer" %in% names(item)) {
+          video_ren <- item[["videoRenderer"]]
+          v <- video_renderer(video = video_ren)
+          if (nrow(v) > 0) {
+            videos <- rbind(videos, v)
+          }
         }
       }
     }
   }
-  print(videos)
+  if (!is.null(videos)) {
+    print(videos)
+  }
 }
+
+youtube_search <- "https://www.youtube.com/youtubei/v1/search"
+response_okay <- 200
+s_quary <- "krishna"
+s_params <- "EgWKAQIQAWoKEAkQChAFEAMQBA%3D%3D"
+
+header <- add_headers(
+  "Content-Type" = "application/json",
+  "prettyPrint"  = "false"
+)
 
 body <- list(
   context = list(
     client = list(
       clientName = "WEB",
-      clientVersion = "1.20241111.01.00"
+      clientVersion = "2.20250219.07.00"
     )
-  ), query = "android", params = "EgWKAQIQAWoKEAkQChAFEAMQBA%3D%3D"
+  ), query = s_quary,
+  continuation = "",
+  params = s_params
 )
 
 format_body <- toJSON(
@@ -157,138 +235,58 @@ format_body <- toJSON(
   pretty = TRUE
 )
 
+interval <- function(i) {
+  cat(":::> Please wait...", i, "sec !\n")
+}
+
 youtube <- POST(
   url = youtube_search,
   config = header,
   body = format_body
 )
 
-temp_vec <- c()
-
 if (status_code(youtube) == response_okay) {
-  search_response <- content(youtube, as = "text", encoding = "UTF-8")
-  result <- fromJSON(search_response)
+  content <- content(youtube, as = "text", encoding = "UTF-8")
+  result <- fromJSON(content)
   content <- contents(result = result)
-  section_lst <- section_list(result = content)
-  next_token <- section_lst
-  contents_result(result = content)
-
-  if (!is.null(next_token)) {
-    while (TRUE) {
-      body <- list(
-        context = list(
-          client = list(
-            clientName = "WEB",
-            clientVersion = "1.20241111.01.00"
-          )
-        ), query = "android", params = "EgWKAQIQAWoKEAkQChAFEAMQBA%3D%3D",
-        continuation = next_token
-      )
-
-      format_body <- toJSON(
-        body,
-        auto_unbox = TRUE,
-        pretty = TRUE
-      )
-
-      youtube <- POST(
-        url = youtube_search,
-        config = header,
-        body = format_body
-      )
-
-      search_response <- content(youtube, as = "text", encoding = "UTF-8")
-      result <- fromJSON(search_response)
-      content <- contents(result = result)
-      section_lst <- section_list(result = content)
-      next_token <- section_lst
-      contents_result(result = content)
-      temp_vec <- c(temp_vec, section_lst)
-      if (is.null(section_lst)) {
-        break()
-      }
-    }
-  }
 }
 
-# ------------------------------------------------------------------------------------
-# library(httr)
-# library(jsonlite)
+tokens <- youtube_token(yt_url = youtube_search, yt_query = s_quary,
+                        yt_params = s_params, yt_body = format_body)
+if (!is.null(tokens)) {
+  i <- 1
+  while (i <= length(tokens)) {
 
-# fetch_continuation <- function(response) {
-#     return(response[["contents"]][["twoColumnSearchResultsRenderer"]]
-#     [["primaryContents"]][["sectionListRenderer"]][["contents"]]
-#     [["continuationItemRenderer"]][["continuationEndpoint"]]
-#     [["continuationCommand"]][["token"]])
-# }
+    body <- list(
+      context = list(
+        client = list(
+          clientName = "WEB",
+          clientVersion = "2.20250219.07.00"
+        )
+      ), query = s_quary,
+      continuation = tokens[i],
+      params = s_params
+    )
 
-# print_token <- function(response) {
-#     print(response)
-# }
+    cat("\n\n\n=====> Token [", i, "]", tokens[i], "\n\n\n")
 
-# tokens <- c()
+    format_body <- toJSON(
+      body,
+      auto_unbox = TRUE,
+      pretty = TRUE
+    )
 
-# youtube_search <- "https://www.youtube.com/youtubei/v1/search"
-# response_okay <- 200
-# s_quary <- "bhagwan mahavir surat"
-# s_params <- "EgWKAQIQAWoKEAkQChAFEAMQBA%3D%3D"
+    youtube <- POST(
+      url = youtube_search,
+      config = header,
+      body = format_body
+    )
 
-# header <- add_headers(
-#     "Content-Type" = "application/json",
-#     "prettyPrint"  = "false"
-# )
-
-# body <- list(
-#     context = list(
-#         client = list(
-#             clientName = "WEB",
-#             clientVersion = "2.20250219.07.00"
-#         )
-#     ), query = s_quary,
-#     continuation = "",
-#     params = s_params
-# )
-
-# format_body <- toJSON(
-#     body,
-#     auto_unbox = TRUE,
-#     pretty = TRUE
-# )
-
-# youtube <- POST(
-#     url = youtube_search,
-#     config = header,
-#     body = format_body
-# )
-
-# result <- fromJSON(content(youtube, as = "text", encoding = "UTF-8"))
-# token <- fetch_continuation(result)[2]
-
-# if (!is.na(token)) {
-#     next_token <- token
-#     duplicated_token <- 1
-#     while (TRUE) {
-#         body[["continuation"]] <- token
-
-#         yt <- POST(
-#             url = youtube_search, config = header, body = format_body
-#         )
-
-#         if (status_code(yt) == response_okay) {
-#             res <- content(yt, as = "text", encoding = "UTF-8")
-#             toLst <- fromJSON(res)
-#             token <- fetch_continuation(toLst)[2]
-#             if (next_token != token) {
-#                 print_token(token)
-#                 next_token <- token
-#             }
-#             if (is.na(token)) {
-#                 break()
-#             }
-#         } else {
-#             print("Response not okay !")
-#         }
-#     }
-# } else {
-#     print("No token found !")
-# }
+    if (status_code(youtube) == response_okay) {
+      content <- content(youtube, as = "text", encoding = "UTF-8")
+      result <- fromJSON(content)
+      content <- contents(result = result)
+    }
+    i <- i + 1
+  }
+}
